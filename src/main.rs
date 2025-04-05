95d4180c7d2604241972c9e5d9be60f757d20772
Dustin Thomas <stdio@cptlobster.dev>
2025-04-04 19:31:51 -0500

    please let this fix this god forsaken build process

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,6 @@
 // You should have received a copy of the GNU General Public License along with
 // this program. If not, see <https://www.gnu.org/licenses/>.
 //
-mod lib;
-
 use clap::Parser;
 use std::io;
 use log::debug;
